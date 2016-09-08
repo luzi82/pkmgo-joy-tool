ca90eb56578d97c32d13c659cac90f85c2670dbf
luzi82 <luzi82@gmail.com>
2016-09-09 01:07:52 +0800

    echo when drone start in bash layer

--- service_drone_dbus.sh
+++ service_drone_dbus.sh
@@ -3,6 +3,7 @@
 source _common.sh
 
 while true; do
+echo "MFVBVEHC drone bash start ${1}"
 python3 src/luzi82/pkmgo/dbus_to_stdout.py luzi82.pkmgo.drone_input \
 | python3 src/luzi82/pkmgo/drone.py -d ${1} \
 | python3 src/luzi82/pkmgo/stdin_to_dbus.py luzi82.pkmgo.drone_output /drone_output
