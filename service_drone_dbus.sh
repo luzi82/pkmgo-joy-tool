ed04b349a72b6d298167eef6cd90996dd018100e
luzi82 <luzi82@gmail.com>
2016-09-10 01:06:58 +0800

    move the data pipe to memcache

--- service_drone_dbus.sh
+++ service_drone_dbus.sh
@@ -6,7 +6,8 @@ while true; do
 echo "MFVBVEHC drone bash start ${1}"
 python3 src/luzi82/pkmgo/dbus_to_stdout.py luzi82.pkmgo.drone_input \
 | python3 src/luzi82/pkmgo/drone.py -d ${1} \
-| python3 src/luzi82/pkmgo/stdin_to_dbus.py luzi82.pkmgo.drone_output /drone_output
+| python3 src/luzi82/pkmgo/stdin_to_memcache_key.py 127.0.0.1:11211 10 \
+| python3 src/luzi82/pkmgo/stdin_to_dbus.py luzi82.pkmgo.drone_output /drone_output_${1}
 if [ "${PIPESTATUS[1]}" == "82" ];then
 break
 fi
--- service_map_gather.sh
+++ service_map_gather.sh
@@ -5,5 +5,6 @@ set -e
 source ./_common.sh
 
 python3 src/luzi82/pkmgo/dbus_to_stdout.py luzi82.pkmgo.drone_output \
+| python3 src/luzi82/pkmgo/memcache_to_stdout.py 127.0.0.1:11211 \
 | python3 src/luzi82/pkmgo/map_gather_filter.py \
 | python3 src/luzi82/pkmgo/stdin_to_memcache.py 127.0.0.1:11211 musashi_map 1200
